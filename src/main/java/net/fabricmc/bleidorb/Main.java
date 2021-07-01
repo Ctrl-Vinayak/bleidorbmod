eabbb1e3a5b3f1a40d427a225f72d695891a9bab
VinvinVinayakvinVin <vinayakshuklakalapnat@live.nl>
2021-07-01 01:02:01 +0100

    version 1.0.3 w.i.p (able to eat "kaas" now)

--- src/main/java/net/fabricmc/bleidorb/Main.java
+++ src/main/java/net/fabricmc/bleidorb/Main.java
@@ -3,15 +3,22 @@ package net.fabricmc.bleidorb;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.bleidorb.items.KaasBlokItem;
 import net.fabricmc.bleidorb.items.KaasItem;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class Main implements ModInitializer {
 
-	public static final Item KAAS_ITEM = new KaasItem(new FabricItemSettings().group(ItemGroup.MISC));
+//	public static final Item KAAS_ITEM = new KaasItem(new FabricItemSettings().group(ItemGroup.FOOD)
+//	.food(new FoodComponent.Builder().hunger(1).saturationModifier(2f).alwaysEdible().build()).maxCount(60));
+	public static final Item KAAS_ITEM = new Item(new Item.Settings().food((new FoodComponent.Builder())
+		.hunger(3)
+		.saturationModifier(0.3F)
+		.build())
+	.maxCount(32));
+
 	public static final Item KAAS_BLOK_ITEM = new KaasBlokItem(new FabricItemSettings().group(ItemGroup.MISC));
 
 	@Override
@@ -27,4 +34,9 @@ public class Main implements ModInitializer {
 		Registry.register(Registry.ITEM, new Identifier("bleidorbmod", "kaas_blok_item"), KAAS_BLOK_ITEM);
 
 	}
+
+	public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
+			new Identifier("bleidorb", "general"), () -> new ItemStack(Items.COBBLESTONE));
+
+
 }
